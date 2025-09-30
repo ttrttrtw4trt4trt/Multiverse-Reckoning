083e8909163393d113fa5ca1980f7296d499828c
thegoatman367 <8972923958@student.cms.k12.nc.us>
2025-09-30 09:35:30 -0400

    Update game.js

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ function drawSquare() {
 }
 
 // Listen for WASD keyboard events only
-document.addEventListener('s', function (event) {
+document.addEventListener('S', function (event) {
   switch (event.key.toLowerCase()) {
     case 'w':
       squareY = Math.max(0, squareY - speed);
