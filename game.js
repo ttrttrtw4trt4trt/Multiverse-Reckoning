97baa772ba1c178f322c63bdb03a8e0c3e629280
thegoatman367 <8972923958@student.cms.k12.nc.us>
2025-09-30 09:18:30 -0400

    game.js

--- game.js
+++ game.js
@@ -1,12 +1,6 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-// Draw a blue square
-ctx.fillStyle = 'blue';
-ctx.fillRect(50, 50, 80, 80);
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
-
 // Square properties
 let squareX = 50;
 let squareY = 50;
