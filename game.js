c73f3f41011a4cec9dcbdcdecc53b57dfbaa7ba3
thegoatman367 <8972923958@student.cms.k12.nc.us>
2025-09-30 14:49:20 -0400

    game.js

--- game.js
+++ game.js
@@ -10,38 +10,69 @@ const jumpStrength = 12;
 let canvasWidth, canvasHeight;
 
 let squareX = 100;
-let squareY = 0; // will be set after resize
+let squareY = 0; // will be set on resize
 let velocityY = 0;
 let onGround = false;
 
 const keysPressed = {};
 
+// Resize and adjust canvas size and square position
 function resizeCanvas() {
-  // Set canvas internal drawing size to match CSS size
-  canvasWidth = window.innerWidth;
-  canvasHeight = window.innerHeight;
+  // If fullscreen, use fullscreen element size, else use window size
+  const width = document.fullscreenElement ? window.screen.width : window.innerWidth;
+  const height = document.fullscreenElement ? window.screen.height : window.innerHeight;
+
+  canvasWidth = width;
+  canvasHeight = height;
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
 
-  // Position square on top of floor after resize
   squareY = canvasHeight - floorHeight - squareSize;
   squareX = Math.min(squareX, canvasWidth - squareSize);
 }
 
 window.addEventListener('resize', resizeCanvas);
+document.addEventListener('fullscreenchange', resizeCanvas);
 resizeCanvas();
 
 document.addEventListener('keydown', (event) => {
-  keysPressed[event.key.toLowerCase()] = true;
-  if (event.key.toLowerCase() === 'w' && onGround) {
+  const key = event.key.toLowerCase();
+  keysPressed[key] = true;
+
+  if (key === 'w' && onGround) {
     velocityY = -jumpStrength;
     onGround = false;
+  }
+
+  // Toggle fullscreen when 'f' pressed
+  if (key === 'f') {
+    toggleFullscreen();
   }
 });
 
 document.addEventListener('keyup', (event) => {
   keysPressed[event.key.toLowerCase()] = false;
 });
+
+function toggleFullscreen() {
+  if (!document.fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) { // Safari
+      canvas.webkitRequestFullscreen();
+    } else if (canvas.msRequestFullscreen) { // IE/Edge
+      canvas.msRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { // Safari
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { // IE/Edge
+      document.msExitFullscreen();
+    }
+  }
+}
 
 function updatePosition() {
   if (keysPressed['a']) {
@@ -65,11 +96,9 @@ function updatePosition() {
 function draw() {
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-  // Draw floor
   ctx.fillStyle = 'green';
   ctx.fillRect(0, canvasHeight - floorHeight, canvasWidth, floorHeight);
 
-  // Draw square
   ctx.fillStyle = 'blue';
   ctx.fillRect(squareX, squareY, squareSize, squareSize);
 }
