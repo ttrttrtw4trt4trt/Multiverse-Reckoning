4ce9624261f788211f2a5a2355d224da1b5b60e1
thegoatman367 <8972923958@student.cms.k12.nc.us>
2025-09-30 09:52:08 -0400

    game.js

--- game.js
+++ game.js
@@ -4,8 +4,8 @@ const ctx = canvas.getContext('2d');
 // Square properties
 const squareSize = 80;
 const speed = 4;
-let squareX = 100;
-let squareY = 100;
+let squareX = -100;
+let squareY = -100;
 
 // Track pressed keys
 const keysPressed = {};
