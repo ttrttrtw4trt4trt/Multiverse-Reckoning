b9f5da84644bdf1d93bba6f7114c6cf94033d71c
thegoatman367 <8972923958@student.cms.k12.nc.us>
2025-09-30 14:39:25 -0400

    game.js

--- game.js
+++ game.js
@@ -1,14 +1,12 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-// Square properties
 const squareSize = 80;
 const speed = 4;
-let squareX = 100;
-let floorHeight = 50;  // height of the floor
-let squareY = canvas.height - floorHeight - squareSize;  // start on top of the floor
+const floorHeight = 50;
 
-// Floor properties
+let squareX = 100;
+let squareY = canvas.height - floorHeight - squareSize;  // Position on top of the floor
 const floorY = canvas.height - floorHeight;
 
 const keysPressed = {};
@@ -30,13 +28,13 @@ function updatePosition() {
   }
 }
 
-function drawSquare() {
+function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
+
   // Draw floor
   ctx.fillStyle = 'green';
   ctx.fillRect(0, floorY, canvas.width, floorHeight);
-  
+
   // Draw square
   ctx.fillStyle = 'blue';
   ctx.fillRect(squareX, squareY, squareSize, squareSize);
@@ -44,13 +42,11 @@ function drawSquare() {
 
 function gameLoop() {
   updatePosition();
-  drawSquare();
+  draw();
   requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
-
-
 
 
 
